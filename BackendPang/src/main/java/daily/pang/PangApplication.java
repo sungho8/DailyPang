ef263d07a8c015e2d3160f0a86c901a4ee9c6329
leeho <leeho@khu.ac.kr>
2022-12-20 22:13:05 +0900

    init: backend initialization

---
+++ BackendPang/src/main/java/daily/pang/PangApplication.java
@@ -0,0 +1,13 @@
+package daily.pang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangApplication.class, args);
+	}
+
+}
---
+++ BackendPang/src/test/java/daily/pang/PangApplicationTests.java
@@ -0,0 +1,13 @@
+package daily.pang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
